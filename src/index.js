44daffeb3775ac90b711953c9382cd93f324f3f9
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-03-23 08:39:17 +0300

    Completed Labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let addTask = document.querySelector("#create-task-form");
+  console.log(addTask);
+  addTask.addEventListener("submit", (e) => {
+    e.preventDefault();
+    onSubmit(e.target.new_task.value);
+  });
 });
+function onSubmit(todo) {
+  let p = document.createElement("p");
+  console.log(p);
+  p.textContent = `${todo}`;
+  let btn = document.createElement("button");
+  btn.textContent = "Delete Task";
+  btn.addEventListener("click", deleteTasks);
+  p.appendChild(btn);
+  console.log(btn);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function deleteTasks(e) {
+  e.target.parentNode.remove();
+}
